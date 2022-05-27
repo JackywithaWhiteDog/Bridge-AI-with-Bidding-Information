7980baf06d0165f5bd642a1c64254745d39d0e8b
jacky <b07705015@ntu.edu.tw>
2022-05-27 13:38:59 +0800

    doc: add help message and update README

--- README.md
+++ README.md
@@ -4,14 +4,45 @@ Final project for Artificial Intelligence (2022 Spring), lectured by [Jane Yung-
 
 ## Dependencies
 
+### Python dependencies
+
 Install the dependencies
 
 ```shell
 pip install -r ./requirements.txt
 ```
 
+### Double Dummy Solver
+
+Follow the [DDS install instruction](https://github.com/dds-bridge/dds/blob/develop/INSTALL) to build the dynamic library and put it into the `./lib` folder. There is already a [compiled file](https://github.com/JackywithaWhiteDog/Bridge-AI-with-Bidding-Information/tree/main/lib) ([v2.9.0](https://github.com/dds-bridge/dds/tree/v2.9.0)) for Linux, build it on your own if you need a specific version or for other platforms.
+
 ## Run the Simulation
+
+Run with default settings:
 
 ```shell
 python main.py
+```
+
+Run `python main.py --help` or `python main.py -h` to get the help:
+
+```text
+usage: main.py [-h] [--declarer_agent {Random,DDS}] [--defender_agent {Random,DDS}] [--num_games NUM_GAMES] [--num_cards_in_hand NUM_CARDS_IN_HAND] [--max_threads MAX_THREADS]
+               [--seed SEED]
+
+Run the matches for bridge AIs
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --declarer_agent {Random,DDS}
+                        The name of declarer agent; currently support Random, DDS. (Default: Random)
+  --defender_agent {Random,DDS}
+                        The name of defender agent; currently support Random, DDS. (Default: DDS)
+  --num_games NUM_GAMES
+                        Number of games to play. (Default: 100)
+  --num_cards_in_hand NUM_CARDS_IN_HAND
+                        Number of cards in players' hands; generally be 13 or 5. (Default: 13)
+  --max_threads MAX_THREADS
+                        Max threads used by DDS; set to 0 for automatically deciding. (Default: 1)
+  --seed SEED           Random seed for reproduction. (Default: None)
 ```
--- main.py
+++ main.py
@@ -8,13 +8,45 @@ from bridge.agents import AGENT_DICT
 
 def parse_args() -> argparse.Namespace:
     """ Parses command line arguments. Returns namespace of arguments."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--declarer_agent', type=str, choices=AGENT_DICT.keys(), default="Random")
-    parser.add_argument('--defender_agent', type=str, choices=AGENT_DICT.keys(), default="Random")
-    parser.add_argument('--num_games', type=int, default=100)
-    parser.add_argument('--num_cards_in_hand', type=int, default=13)
-    parser.add_argument('--max_threads', type=int, default=1)
-    parser.add_argument('--seed', type=int, default=None)
+    parser = argparse.ArgumentParser(description='Run the matches for bridge AIs')
+    parser.add_argument(
+        '--declarer_agent',
+        type=str,
+        choices=AGENT_DICT.keys(),
+        default="Random",
+        help="The name of declarer agent; currently support Random, DDS. (Default: Random)"
+    )
+    parser.add_argument(
+        '--defender_agent',
+        type=str,
+        choices=AGENT_DICT.keys(),
+        default="DDS",
+        help="The name of defender agent; currently support Random, DDS. (Default: DDS)"
+    )
+    parser.add_argument(
+        '--num_games',
+        type=int,
+        default=100,
+        help="Number of games to play. (Default: 100)"
+    )
+    parser.add_argument(
+        '--num_cards_in_hand',
+        type=int,
+        default=13,
+        help="Number of cards in players' hands; generally be 13 or 5. (Default: 13)"
+    )
+    parser.add_argument(
+        '--max_threads',
+        type=int,
+        default=1,
+        help="Max threads used by DDS; set to 0 for automatically deciding. (Default: 1)"
+    )
+    parser.add_argument(
+        '--seed',
+        type=int,
+        default=None,
+        help="Random seed for reproduction. (Default: None)"
+    )
     return parser.parse_args()
 
 def set_seed(seed):
